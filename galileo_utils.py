21955b080ec60f90de946336c4612e8b04fd6fa9
Javier Goday <jgoday@gmail.com>
2015-02-02 07:59:29 +0100

    UI Changes

--- galileo_utils.py
+++ galileo_utils.py
@@ -3,21 +3,24 @@ import galileo
 import galileo.main
 
 class GalileoLogHandler(logging.Handler):
-    def __init__(self, error_fn):
+    def __init__(self, error_fn, info_fn):
         logging.Handler.__init__(self)
 
         self.error_fn = error_fn
+        self.info_fn = info_fn
 
     def emit(self, record):
         if record.levelno == logging.ERROR :
             self.error_fn(record.getMessage())
+        elif record.levelno == logging.INFO :
+            self.info_fn(record.getMessage())
 
-def start_galileo(error_fn):
+def start_galileo(error_fn, info_fn):
     logging.basicConfig(level=logging.DEBUG)
 
     logger = logging.getLogger()
 
-    handler = GalileoLogHandler(error_fn)
+    handler = GalileoLogHandler(error_fn, info_fn)
 
     logging.getLogger(galileo.__name__).addHandler(handler)
 
--- main_window.py
+++ main_window.py
@@ -1,39 +1,61 @@
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 from galileo_utils import start_galileo
 
 class MainWindow(Gtk.Window):
+    BLUE_COLOR = Gdk.RGBA(.1, .1, .222, 1)
     def __init__(self):
         Gtk.Window.__init__(self, title="Fitbit connect")
-        self.set_default_size(400, 200)
+        self.set_default_size(400, 300)
 
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
         hb.props.title = "Fitbit connect"
         self.set_titlebar(hb)
 
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        self.add(box)
+        vbox = Gtk.VBox(spacing=0)
+        self.add(vbox)
 
         self.label = Gtk.Label()
-        box.pack_start(self.label, True, True, 10)
+        self._set_background_color(self.label, self.BLUE_COLOR)
+        vbox.pack_start(self.label, True, True, 0)
 
+        hbox = Gtk.HBox()
         self.btn = Gtk.Button(label="Connect")
-        box.pack_start(self.btn, True, True, 10)
+        hbox.pack_end(self.btn, False, False, 10)
+        vbox.pack_start(hbox, False, False, 10)
+        self.btn.set_size_request(40, 30)
 
+        # events and actions
         self.btn.connect("clicked", self.on_btn_clicked)
         self.connect("delete-event", Gtk.main_quit)
 
-        self.btn.set_sensitive(False)
-        self.galileo = start_galileo(self.on_error)
+        # launch main galileo function
+        self._disable_actions()
+        # self.galileo = start_galileo(self.on_error, self.on_info)
 
     def on_btn_clicked(self, btn):
-        self.btn.set_sensitive(False)
+        self._disable_actions()
         self.galileo.main()
-
 
     def on_error(self, text):
         self.label.set_text(text)
+        self._enable_actions()
+
+    def on_info(self, text):
+        self.label.set_text(text)
+        self._enable_actions()
+
+
+
+    def _disable_actions(self):
+        self.btn.set_sensitive(False)
+
+    def _enable_actions(self):
         self.btn.set_sensitive(True)
+
+    def _set_background_color(self, widget, color):
+        widget.override_background_color(Gtk.StateFlags.NORMAL, color)
+
 
 if __name__ == "__main__" :
     win = MainWindow()
